4d445335b4da0525d12c62ebc3fa4b7bb4dcda6a
HitaarnavBhatia <s4898402@student.uq.edu.au>
2025-12-04 16:50:35 +0530

    added the router to link the pages and created a navbar to show both the student and course page at the begining

--- src/App.jsx
+++ src/App.jsx
@@ -1,160 +1,21 @@
-import React, { useState, useEffect } from "react";
-import "./index.css";
-import "./App.css";
-import studentsData from "./students.json";
-import StudentForm from "./studentForm";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import StudentsPage from "./StudentPage";
+import CoursesPage from "./CoursePage";
 
 function App() {
-
-  //initialized local storage, i.e it loads data from localStorage if available otherwise uses json
-  let initialStudents;
-  const storedStudents = localStorage.getItem("students");
-
-  if (storedStudents) {
-    //if stored array exists or is not null
-    initialStudents = JSON.parse(storedStudents); //convert strign to java object using JSON.parse [bcs localStorage saves everything as strings]
-  } else {
-    // if null, then use studentsData which is imported from students.json file
-    initialStudents = studentsData;
-  }
-  const [students, setStudents] = useState(initialStudents);
-
-  const [name, setName] = useState("");
-  const [roll, setRoll] = useState("");
-  const [course, setCourse] = useState("");
-
-  const [isStudentEditing, setIsStudentEditing] = useState(false);
-  const [studentIndex, setStudentIndex] = useState(-1);
-
-  //saves students data to locaStorage whenevr its updated
-  useEffect(
-    () => localStorage.setItem("students", JSON.stringify(students)),
-    [students]
-  );
-
-  //------ADD Student---------
-  function addStudent(e) {
-    e.preventDefault();
-
-    let rollExists = false;
-
-    for (let i = 0; i < students.length; i++) {
-      if (isStudentEditing && i === studentIndex) continue;
-      if (students[i].roll === roll) {
-        rollExists = true;
-        break;
-      }
-    }
-
-    if (rollExists) {
-      toast.error("Roll number already exists!");
-      return;
-    }
-
-    if (isStudentEditing) {
-      // Update existing student
-      const updatedStudents = students.slice();
-      updatedStudents[studentIndex].name = name;
-      updatedStudents[studentIndex].roll = roll;
-      updatedStudents[studentIndex].course = course;
-      setStudents(updatedStudents);
-
-      toast.success("Student updated successfully!");
-      setIsStudentEditing(false);
-      setStudentIndex(-1);
-    } else {
-      const newStudent = {
-        id: `S00${students.length + 1}`,
-        name,
-        roll,
-        course,
-      };
-
-      setStudents([...students, newStudent]);
-      toast.success("Student added successfully!");
-    }
-
-    setName("");
-    setRoll("");
-    setCourse("");
-
-  }
-
-  //------DELETE Student---------
-  function deleteStudent(index) {
-    const newStudents = students.slice();
-    newStudents.splice(index, 1);
-    setStudents(newStudents);
-
-    toast.success("Student deleted!");
-  }
-
-  //------EDIT Student---------
-  function editStudent(index) {
-    const student = students[index];
-    setName(student.name);
-    setRoll(student.roll);
-    setCourse(student.course);
-    setIsStudentEditing(true);
-    setStudentIndex(index);
-  }
-
   return (
-    <div className="p-8">
-      <ToastContainer />
+    <BrowserRouter>
+      <nav className="p-4 flex gap-6 bg-gray-200">
+        <Link to="/student">Students</Link>
+        <Link to="/course">Courses</Link>
+      </nav>
 
-      <h1 className="text-4xl font-bold mb-6 text-center">Student Table</h1>
-
-      <StudentForm
-        name={name}
-        roll={roll}
-        course={course}
-        setName={setName}
-        setRoll={setRoll}
-        setCourse={setCourse}
-        addStudent={addStudent}
-        isEditing={isStudentEditing}
-      />
-
-      <table className="border border-gray-400 w-full shadow-lg">
-        <thead className="bg-gray-100">
-          <tr>
-            <th className="border-2 px-4 py-2">ID</th>
-            <th className="border-2 px-4 py-2">Name</th>
-            <th className="border-2 px-4 py-2">Roll Number</th>
-            <th className="border-2 px-4 py-2">Course Name</th>
-            <th className="border-2 px-4 py-2">Actions</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          {students.map((student, index) => (
-            <tr key={student.id}>
-              <td className="border px-4 py-2">{student.id}</td>
-              <td className="border px-4 py-2">{student.name}</td>
-              <td className="border px-4 py-2">{student.roll}</td>
-              <td className="border px-4 py-2">{student.course}</td>
-              <td className="border px-4 py-2">
-                <button
-                  onClick={() => editStudent(index)}
-                  className="bg-blue-600 hover:bg-blue-700 text-white px-3 py-1 rounded"
-                >
-                  Edit
-                </button>
-                <button
-                  onClick={() => deleteStudent(index)}
-                  className="bg-red-600 hover:bg-red-700 text-white px-3 py-1 rounded"
-                >
-                  Delete
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+      <Routes>
+        <Route path="/student" element={<StudentsPage />} />
+        <Route path="/course" element={<CoursesPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import App from './App.jsx'
 import './index.css'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
