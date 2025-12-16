299553921803e0b440f2c285507608a17f6831e0
Hitaarnav Bhatia <hitaarnavbhatia@Hitaarnavs-MacBook-Air.local>
2025-12-16 16:11:11 +0530

    server test

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,11 @@
 const express = require("express");
-const cors = require("cors");
-const logger = require("./logger");
-
-// ROUTES
-const studentRoutes = require("./routes/studentRoutes");
-const courseRoutes = require("./routes/courseRoutes");
-
 const app = express();
 
-app.use(cors());
-app.use(express.json());
-
 app.get("/", (req, res) => {
-  logger.info("Home route accessed");
-  res.send("Backend is running correctly!");
+  res.send("OK");
 });
 
-// ROUTES
-app.use("/students", studentRoutes);
-app.use("/courses", courseRoutes);
-
-// START SERVER
-const PORT = process.env.PORT || 4000;
-
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  logger.info(`Backend running on port ${PORT}`);
-  console.log(`Backend running on port ${PORT}`);
+  console.log("OK server listening on", PORT);
 });
