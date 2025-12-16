3015a491302dbf505122e0e19bf6e8242078665f
Hitaarnav Bhatia <hitaarnavbhatia@Hitaarnavs-MacBook-Air.local>
2025-12-16 14:41:10 +0530

    corrected some code

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,11 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  user: "postgres",
+  host: "trolley.proxy.rlwy.net",
+  database: "railway",
+  password: "LInCyeAQbpPrbuKKrKgaXoqDBbkhPJtm",
+  port: 1
 });
 
 module.exports = pool;
