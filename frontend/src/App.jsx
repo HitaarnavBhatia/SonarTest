3e45b897aec5cce85ad23ffca78e9304e4203b7c
Hitaarnav Bhatia <hitaarnavbhatia@Hitaarnavs-MacBook-Air.local>
2025-12-17 16:23:36 +0530

    changed app.jsx added navbar landing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,49 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, NavLink, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
 import StudentsPage from "./StudentPage";
 import CoursesPage from "./CoursePage";
+import HomePage from "./HomePage";
 
 function App() {
   return (
     <BrowserRouter>
-      <nav className="p-4 bg-gray-100/60 backdrop-blur-md shadow-md border-b border-gray-300 flex justify-center rounded-4xl">
+      {/* NAVBAR */}
+      <nav className="p-4 bg-gray-100/60 backdrop-blur-md shadow-md border-b border-gray-300 flex justify-center">
         <div className="flex gap-8 text-lg font-medium">
-          <NavLink to="/student"
-            className={({ isActive }) => `px-4 py-2 rounded-lg transition ${isActive
-              ? "bg-blue-600 text-white shadow"
-              : "text-gray-700 hover:bg-gray-200"}`
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              `px-4 py-2 rounded-lg transition ${
+                isActive
+                  ? "bg-blue-600 text-white shadow"
+                  : "text-gray-700 hover:bg-gray-200"
+              }`
+            }
+          >
+            Home
+          </NavLink>
+
+          <NavLink
+            to="/student"
+            className={({ isActive }) =>
+              `px-4 py-2 rounded-lg transition ${
+                isActive
+                  ? "bg-blue-600 text-white shadow"
+                  : "text-gray-700 hover:bg-gray-200"
+              }`
             }
           >
             Students
           </NavLink>
-          <NavLink to="/course"
-            className={({ isActive }) => `px-4 py-2 rounded-lg transition ${isActive
-              ? "bg-blue-600 text-white shadow"
-              : "text-gray-700 hover:bg-gray-200"}`
+
+          <NavLink
+            to="/course"
+            className={({ isActive }) =>
+              `px-4 py-2 rounded-lg transition ${
+                isActive
+                  ? "bg-blue-600 text-white shadow"
+                  : "text-gray-700 hover:bg-gray-200"
+              }`
             }
           >
             Courses
@@ -27,8 +51,9 @@ function App() {
         </div>
       </nav>
 
+      {/* ROUTES */}
       <Routes>
-        <Route path="/" element={<Navigate to="/student" />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/student" element={<StudentsPage />} />
         <Route path="/course" element={<CoursesPage />} />
       </Routes>
